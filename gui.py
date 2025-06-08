db2d75c102f6bcfb88ee88af6214f6195f08596a
Divyansh Singh <divyanshsingh@Divyanshs-MacBook-Pro.local>
2025-06-08 15:42:25 +0530

    Testing git connection 4

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from stress_test import start_test
 def create_gui():
 
     root = tk.Tk()
-    root.title("CPU Stress Testing Tool")
+    root.title("CPU Stress Tool")
 
 
     instructions = tk.Label(root, text="Instructions:\n"
